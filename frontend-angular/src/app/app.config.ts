83b32fb7eff8014c2eadbf493864c5e73ce12871
bolatah <bolatah@outlook.com>
2025-04-13 23:16:09 +0200

    change the api url in the frontend

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { environment } from '../environment';
+import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { authInterceptor } from './auth.interceptor';
--- frontend-angular/src/app/services/nest.service.ts
+++ frontend-angular/src/app/services/nest.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Nest {
   id?: string;
@@ -12,7 +13,7 @@ export interface Nest {
   providedIn: 'root',
 })
 export class NestService {
-  private apiUrl = 'http://localhost:8080/api/nests'; 
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
