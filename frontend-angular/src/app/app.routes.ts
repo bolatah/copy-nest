a84d2170f1f3caf1fcf9ca4184f5407365a5e20b
bolatah <bolatah@outlook.com>
2025-04-15 16:34:15 +0200

    add privacy route and component in the frontend

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { NestContentComponent } from './nest-content/nest-content.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './guards/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent }, // Public
+    { path: 'privacy-policy', component: PrivacyPolicyComponent }, // Public
     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }, 
     { path: 'offline', component: OfflineComponent, canActivate: [authGuard] }, // Private
     { path: '', redirectTo: 'login', pathMatch: 'full' },
--- frontend-angular/src/app/login/login.component.ts
+++ frontend-angular/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export type AuthMode = 'signIn' | 'signUp' | 'forgotPassword';
@@ -9,7 +9,7 @@ export type AuthMode = 'signIn' | 'signUp' | 'forgotPassword';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
