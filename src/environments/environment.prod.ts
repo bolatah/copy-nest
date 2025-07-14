e122fc3c6733ef8bc08c25cf7bf531e3b3f077df
bolatah <bolatah@outlook.com>
2025-07-14 20:55:57 +0200

    add environment files for angular build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: true,
+   apiUrl: 'https://copy-nest-backend-aba59dd17789.herokuapp.com/api/nests',
+  firebaseConfig: {
+  "apiKey": "",
+  "authDomain": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
+}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: false,
+   apiUrl: 'http://localhost:8080/api/nests',
+  firebaseConfig: {
+  "apiKey": "",
+  "authDomain": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
+}
+};
